b58080a1b2e50aa6caa488ed53468d75f0b4ac22
pooneh <poonehshooshtari@gmail.com>
2017-05-07 12:24:58 -0600

    Added functions to read JSON data from front end (primitives data)

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 
-from Vector import Vector 
+from Vector import Vector
+from Sphere import Sphere
+from Triangle import Triangle 
 from PointLight import PointLight
 from Screen2D import Screen2D
 from RayTracer import RayTracer
@@ -7,6 +9,53 @@ from flask import Flask, render_template, request, jsonify, Response, send_file,
 from flask_celery import make_celery
 import re 
 import base64, time
+
+
+#helper function to use regex in order to read tuple (vector) elements which is given as a string 
+def vectorElem(vector):
+	vectorElements = re.findall("[-+]?\d*\.\d+|[-+]?\d+", vector)
+	print "vectorElem", vectorElements
+	print len(vectorElements)
+	if len(vectorElements) != 0:
+		return float(vectorElements[0]), float(vectorElements[1]), float(vectorElements[2]) 
+
+
+"""helper function to read JSON data from dynamic Form and returns the primitive 
+objects which are chosen by the user"""
+
+def readDynamicForm(dynamicFormData):
+	primitiveObjs = []
+	for item in dynamicFormData:
+		for key in item:
+			if key == "sphere":
+				sphere_position = item[key]['sphere-position']
+				pos_x, pos_y, pos_z = vectorElem(sphere_position)
+				radius = float(item[key]['radius'])
+				print(str(item[key]['color']))
+				color = str(item[key]['color'])
+				print "color", color
+				R, G, B = vectorElem(color)
+				print "R", R, G, B
+
+				material = str(item[key]['material'])
+				print material
+
+				sphere = Sphere(position=Vector(pos_x, pos_y, pos_z), radius=radius, ka=0, kd=0, material=material) #color=Vector(R, G, B)
+				primitiveObjs.append(sphere)
+
+			elif key == "triangle":
+				vertexA = item[key]['vertexA']
+				verA_x, verA_y, verA_z = vectorElem(vertexA)
+				vertexB = item[key]['vertexB']
+				verB_x, verB_y, verB_z = vectorElem(vertexB)
+				vertexC = item[key]['vertexC']
+				verC_x, verC_y, verC_z = vectorElem(vertexC)
+				color = str(item[key]['color'])
+				R, G, B = vectorElem(color)
+				material = str(item[key]['material'])
+				triangle = Triangle(a= Vector(verA_x, verA_y, verA_z), b= Vector(verB_x, verB_y, verB_z), c= Vector(verC_x, verC_y, verC_z), color=Vector(R, G, B), ka = 0, kd = 0, material=material)
+				primitiveObjs.append(triangle)
+	return primitiveObjs
 
 
 # create a Flask instance and initialize the flask application
@@ -26,7 +75,7 @@ def my_form():
 
 
 
-@app.route('/result', methods=['GET', 'POST'])
+@app.route('/result', methods=['POST'])
 def result():
 	
 	# light_position = request.args.get('light_position',"(0, 0, 0)", type=str)  
@@ -37,19 +86,21 @@ def result():
 
 	
 	light_position = request.json["lightPosition"]
-	image_size = request.json["imageSize"]
-	dynamicForm = request.json["dynamicForm"]
-	print "dynamicForm", dynamicForm
-	print "imageSize", image_size
-	print "light_position", light_position
-	light_position_coor = re.findall("[-+]?\d*\.\d+|[-+]?\d+", light_position)
+	image_size = int(request.json["imageSize"])
+	ambient_illumination = float(request.json["ambIllumination"])
 
-	x = float(light_position_coor[0])
-	y = float(light_position_coor[1])
-	z = float(light_position_coor[2])
+	dynamicFormData = request.json["dynamicForm"]
+	print "dynamicFormData", dynamicFormData
+	primitiveObjs = readDynamicForm(dynamicFormData)
+	# print "objects", primitiveObjs
+	# print type(primitiveObjs)
+	# print len(primitiveObjs)
+
+	
+	x, y, z = vectorElem(light_position)
 
 	pointlight = PointLight(position=Vector(x, y, z), color=Vector(255, 255, 255), Ka=ambient_illumination) 
-	raytracer = RayTracer(screen2D=[image_size, image_size], lights=[pointlight], camerapos=Vector(7.5, 5, 10), O=Vector(0, 0, 0), U=Vector(10, 0, 0), V=Vector(0, 10, 0)) 
+	raytracer = RayTracer(screen2D=[image_size, image_size], primitives=primitiveObjs, lights=[pointlight], camerapos=Vector(7.5, 5, 10), O=Vector(0, 0, 0), U=Vector(10, 0, 0), V=Vector(0, 10, 0)) 
 	raytracer.render_image()
 
 	# the image is encoded to base64 which returns a string (encoded_string)
