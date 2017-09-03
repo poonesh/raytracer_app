18ba1be54cc9a4fd54b8a6991860fbb3d921605a
pooneh <poonehshooshtari@gmail.com>
2017-09-02 18:50:32 -0600

    Organized the app and added login manager

--- app.py
+++ app.py
@@ -3,13 +3,14 @@ import socket
 gevent.monkey.patch_all()
 if socket.socket is gevent.socket.socket:
     print "gevent monkey patch has occurred"
-from flask import Flask, render_template, request, json, jsonify
-from flask_socketio import SocketIO, emit, send, join_room, leave_room, close_room, rooms, disconnect
-from flask_celery import make_celery
-from celery import Celery
-import urllib2
-import re
-import base64, time, cStringIO
+
+from flask import Flask, session, render_template, flash, redirect, url_for, request, json, jsonify
+import urllib2, base64, time, cStringIO, re
+
+from config import set_config
+from app_helper_functions import vectorElem, readDynamicForm
+from forms import *
+
 from Vector import Vector
 from Sphere import Sphere
 from Triangle import Triangle
@@ -18,74 +19,47 @@ from Screen2D import Screen2D
 from RayTracer import RayTracer
 
 
-#helper function to use regex in order to read tuple (vector) elements which is given as a string
-def vectorElem(vector):
-	vectorElements = re.findall("[-+]?\d*\.\d+|[-+]?\d+", vector)
-	print "vectorElem", vectorElements
-	print len(vectorElements)
-	if len(vectorElements) != 0:
-		return float(vectorElements[0]), float(vectorElements[1]), float(vectorElements[2])
-
-
-"""helper function to read JSON data from dynamic Form and returns the primitive
-objects which are chosen by the user"""
-
-def readDynamicForm(dynamicFormData):
-	primitiveObjs = []
-	for item in dynamicFormData:
-		for key in item:
-			if key == "sphere":
-				sphere_position = item[key]['sphere-position']
-				pos_x, pos_y, pos_z = vectorElem(sphere_position)
-				color = str(item[key]['color'])
-				R, G, B = vectorElem(color)
-				radius = float(item[key]['radius'])
-				material = str(item[key]['material'])
-				sphere = Sphere(position=Vector(pos_x, pos_y, pos_z), color=Vector(R, G, B), radius=radius, ka=0, kd=0, material=material) 
-				primitiveObjs.append(sphere)
-
-			elif key == "triangle":
-				vertexA = item[key]['vertexA']
-				verA_x, verA_y, verA_z = vectorElem(vertexA)
-				vertexB = item[key]['vertexB']
-				verB_x, verB_y, verB_z = vectorElem(vertexB)
-				vertexC = item[key]['vertexC']
-				verC_x, verC_y, verC_z = vectorElem(vertexC)
-				color = str(item[key]['color'])
-				R, G, B = vectorElem(color)
-				material = str(item[key]['material'])
-				triangle = Triangle(a=Vector(verA_x, verA_y, verA_z), b=Vector(verB_x, verB_y, verB_z), c=Vector(verC_x, verC_y, verC_z), color=Vector(R, G, B), ka=0, kd=0, material=material)
-				primitiveObjs.append(triangle)
-	return primitiveObjs
-
-
 # create a Flask instance and initialize the flask application
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
+socketio, celery = set_config(app)
 
-app.config.update(
-CELERY_BROKER_URL='amqp://localhost//',
-CELERY_RESULT_BACKEND='amqp://localhost//'
-)
-
-socketio = SocketIO(app, engineio_logger=True, ping_timeout=120, message_queue='amqp://')
-celery = make_celery(app)
-
+# set homepage to form.htm
 @app.route('/')
 def my_form():
     return render_template("form.html")
 
 
 # route for handling login page
-@app.route('/login', method=['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-	error = None
-	if request.method == 'POST':
-		if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-			error = 'Invalid Credentials. Please try again.'
-		else:
-			return redirect(url_for('home'))
-	return render_template('login.html', error=error)
+	form = LoginForm()
+	if form.validate_on_submit():
+		user = User.query.filter_by(username=form.username.data).first()
+		if user:
+			if check_password_hash(user.password, form.password.data):
+				return redirect(url_for('my_form'))
+			return '<h1>Invalid username or password</h1>'
+		# return '<h1>' + form.username.data + ' ' + form.password.data + '</h1>'
+	return render_template('login.html', form=form)
+
+
+@app.route('/signup', methods=['GET','POST'])
+def signup():
+	form = RegisterForm()
+	if form.validate_on_submit():
+		hashed_password = generate_password_hash(form.password.data, method="sha256")
+		new_user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+		db.session.add(new_user)
+		db.session.commit()
+		return '<h1>New User Has Been Created!</h1>'
+		# return '<h1>' + form.username.data + ' ' +form.email.data + ' ' + form.password.data + '</h1>'
+	return render_template('signup.html', form=form)
+
+
+@app.route('/logout')
+def logout():
+	logout_user()
+	return redirect(url_for('my_form'))
 
 
 @app.route('/result', methods=['POST'])
@@ -97,7 +71,6 @@ def result():
 	final_result.delay(light_position, image_size, ambient_illumination, dynamicFormData)
 	return jsonify({"status": 200})
 	
-
 
 @celery.task(name='app.result')
 def final_result(light_position, image_size, ambient_illumination, dynamicFormData):
@@ -118,9 +91,8 @@ def final_result(light_position, image_size, ambient_illumination, dynamicFormDa
 
 	raytracer.render_image(call_back_func1, call_back_func2)
 
-	   
+
 if __name__ == '__main__':
     app.debug = True
-    socketio.run(app, port=5000)
+    socketio.run(app, port=5000) 
 
-    
