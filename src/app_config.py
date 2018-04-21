95db5c551304be4a0e1c4a86729bd0491158d081
Ubuntu <ubuntu@ip-172-31-47-85.us-east-2.compute.internal>
2018-04-21 20:15:49 +0000

    Fix leftover hard-coded database password

--- src/app_config.py
+++ src/app_config.py
@@ -1,13 +1,16 @@
+import os
 from flask_socketio import SocketIO
 from flask_celery import make_celery
 
 def set_config(app):
 	app.config['SECRET_KEY'] = 'secret!'
+	password = os.environ['POSTGRES_PWD']
+
 	app.config.update(
 	# Broker settings
 	CELERY_BROKER_URL='amqp://localhost//',
 	# Using the database to store task state and results
-	CELERY_RESULT_BACKEND='db+postgresql://postgres:NewDatabase@localhost/user_data'
+	CELERY_RESULT_BACKEND='db+postgresql://postgres:{}@localhost/user_data'.format(password)
 	)
 
 	socketio = SocketIO(app, engineio_logger=True, ping_timeout=120, message_queue='amqp://')
